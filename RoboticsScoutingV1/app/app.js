710ce6c8ac9dbbdcaa03aa731a80d88a10718a25
ocampossoto <ocampossoto1@gmail.com>
2018-12-17 01:00:01 -0600

    Basic outline of the webpage. With navbar, homepage, about,  and  a contatct page.

--- RoboticsScoutingV1/app/app.js
+++ RoboticsScoutingV1/app/app.js
@@ -1,6 +1,16 @@
-﻿var app = angular.module('myApp', []);
-
-app.controller('myCtrl', function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+﻿var app = angular.module('myApp', ["ngRoute"]);
+//Allows for routing between diffrent pages without reloading the page. 
+//html files are just divs with what you want on that page. 
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.html"
+        })
+        .when("/about", {
+            templateUrl: "about.html"
+        })
+        .when("/contact", {
+            templateUrl: "contact.html"
+        });
 });
+
