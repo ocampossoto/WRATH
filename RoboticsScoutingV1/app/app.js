884b342909c5bffdd6baae06586c02b78cdaf028
ocampossoto <ocampossoto1@gmail.com>
2018-12-27 19:56:32 -0600

    Fixed some stuff set up an update.

--- RoboticsScoutingV1/app/app.js
+++ RoboticsScoutingV1/app/app.js
@@ -12,67 +12,53 @@ app.config(function ($routeProvider) {
         .when("/contact", {
             templateUrl: "/pages/contact.html"
         })
+        .when("/scouting1", {
+            templateUrl: "/pages/scouting1.html"
+        })
         .when("/scouting", {
             templateUrl: "/pages/scouting.html"
         });
 });
+//comments mean old code that is due for deletion later on
 
+/////this code has two itterations 12/27/2018 /// delete after next update
 //app.controller('myCtrl', function ($scope) {
 //    $scope.firstName = "John";
 //    $scope.lastName = "Doe";
 //});
 
-app.controller("SlideShowController", function ($scope) {
-    $scope.slides = [{
-            imageUrl: "../images/doubt.jpg", //adding image once Oscar chooses them
-            caption: "caption goes here" //option caption, not sure if we'll use them
-    }, {
-            imageUrl: "../images/Robotics_logo.jpg",
-            caption: "caption goes here"
-        //can always add more later
-    }];
-});
-app.directive("slideShow", function () {
-    return {
-        restrict: 'AE',
-        transclude: true,
-        scope: {
-            slides: '='
-        },
-        template: `
-      <div class="slideshow">
-        <ul class="slideshow-slides">
-        <li ng-repeat="slide in slides" ng-class="{ active: $index == activeIndex }">
-          <figure>
-            <img ng-src="{{ slide.imageUrl}}" />
-            <figcaption ng-show="slide.caption">{{ slide.caption }}</figcaption>
-          </figure>
-        </li>
-        </ul>
-        <ul class="slideshow-dots">
-          <li ng-repeat="slide in slides" ng-class="{ active: $index == activeIndex }">
-            <a ng-click="jumpToSlide($index)">{{ $index + 1 }}</a>
-          </li>
-        </ul>
-      </div>
-    `,
-        link: function ($scope, element, attrs) {
-            var timer = null;
-            $scope.activeIndex = 0;
-
-            $scope.jumpToSlide = function (index) {
-                $scope.activeIndex = index;
-                restartTimer();
-            };
-
-        }
-    };
-
-});
-
-var root = document.querySelector('#root');
-angular.element(root).ready(function () {
-    angular.bootstrap(root, ['app']);
-});
+/////irvins stuff moved to MainController.js 12/27/2018 0 itterations
+//app.controller("SlideShowController", function ($scope) {
+//    $scope.slides = [{
+//            imageUrl: "../images/doubt.jpg", //adding image once Oscar chooses them
+//            caption: "caption goes here" //option caption, not sure if we'll use them
+//    }, {
+//            imageUrl: "../images/Robotics_logo.jpg",
+//            caption: "caption goes here"
+//        //can always add more later
+//    }];
+//});
+//app.directive("slideShow", function () {
+//    return {
+//        restrict: 'AE',
+//        transclude: true,
+//        scope: {
+//            slides: '='
+//        },
+//        templateUrl: "../pages/slideshow.html",
+//        link: function ($scope, element, attrs) {
+//            var timer = null;
+//            $scope.activeIndex = 0;
+//            $scope.jumpToSlide = function (index) {
+//                $scope.activeIndex = index;
+//                restartTimer();
+//            };
+//        }
+//    };
+//});
+//var root = document.querySelector('#root');
+//angular.element(root).ready(function () {
+//    angular.bootstrap(root, ['app']);
+//});
 
 
--- RoboticsScoutingV1/app/controllers/MainController.js
+++ RoboticsScoutingV1/app/controllers/MainController.js
@@ -1,4 +1,35 @@
 ﻿app.controller('myCtrl', function ($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
+    //irvins slide show 
+    $scope.slides = [{
+        imageUrl: "../images/doubt.jpg", //adding image once Oscar chooses them
+        caption: "caption goes here" //option caption, not sure if we'll use them
+    }, {
+        imageUrl: "../images/Robotics_logo.png",
+        caption: "caption goes here"
+        //can always add more later
+    }];
+});
+
+app.directive("slideShow", function () {
+    return {
+        restrict: 'AE',
+        transclude: true,
+        scope: {
+            slides: '='
+        },
+        templateUrl: "../pages/slideshow.html",
+        link: function ($scope, element, attrs) {
+            var timer = null;
+            $scope.activeIndex = 0;
+
+            $scope.jumpToSlide = function (index) {
+                $scope.activeIndex = index;
+                restartTimer();
+            };
+
+        }
+    };
+
 });
